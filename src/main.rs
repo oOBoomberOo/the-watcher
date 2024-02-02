af1e3042fd41dc7949dcaabb919c74211c4f0261
oOBoomberOo <boomberisalreadytaken@gmail.com>
2024-02-02 21:21:57 +0900

    Removed REPL because it's too fragile.
    Might revisit this feature later.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-use the_watcher::repl::{self, Repl, ReplError};
-
 #[tokio::main]
-async fn main() -> Result<(), ReplError> {
-    tracing_subscriber::fmt().pretty().init();
-    let mut repl = Repl::new()?;
-
-    if let Err(err) = repl::start(&mut repl).await {
-        eprintln!("{}", err);
-        std::process::exit(1);
-    }
-
-    Ok(())
+async fn main() {
+    println!("Hello, world!");
 }
